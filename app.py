38c20c057e254750e4510a4fcfac7277bf8ee2f1
Kushal Guha5 <64835424+kushalguha5@users.noreply.github.com>
2023-09-30 13:55:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ region_df = pd.read_csv('noc_regions.csv')
 
 df = preprocessor.preprocess(df,region_df)
 
-st.sidebar.title("Olympics Analysis Visualization")
+st.sidebar.title("Olympics Analysis Visualization Tool by Kushal Guha")
 st.sidebar.image('https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')
 user_menu = st.sidebar.radio(
     'Select an Option',
